388db5c6f8fa199a3e18fd6b3463939ed7e3e27c
epenick123 <baseball_man25852@hotmail.com>
2025-04-01 13:14:41 -0700

    C1L11: readiness endpoint

--- main.go
+++ main.go
@@ -8,16 +8,28 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
+	mux.HandleFunc("/healthz", healthzHandler)
+
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	w.WriteHeader(200)
+
+	w.Write([]byte("OK"))
 
 }
