9cfb864cd8609b6bbe1a71c43f218aac5e445c08
epenick123 <baseball_man25852@hotmail.com>
2025-04-01 12:28:15 -0700

    lesson 4, server setup

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
